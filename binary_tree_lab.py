f2c9a4d1e3d5c8121158d3d73bf9a35f694e3247
Leon Muigai <leonmuigaike@gmail.com>
2026-07-30 21:01:49 +0300

    Handle right subtree case for lowest common ancestor

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -22,3 +22,15 @@ def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode
 
     if p.val < root.val and q.val < root.val:
         return lowest_common_ancestor(root.left, p, q)
+
+    if p.val > root.val and q.val > root.val:
+        return lowest_common_ancestor(root.right, p, q)
+
+
+
+
+
+
+
+
+
